24ba7bb9db8ae05f1082cae29f0aa094e205c51f
Jonathan Howard <j@hovverd.com>
2014-05-10 16:20:59 -0400

    Twitter streaming service up and running–now saves tweets to mongoDB
    Former-commit-id: d65c3f1c0f892985f5f0066852e78bad8c7c29b9

--- streaming.py
+++ streaming.py
@@ -1,6 +1,14 @@
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+from tweepy.utils import import_simplejson
+
+from pymongo import MongoClient
+
+mongoc = MongoClient()
+db = mongoc.natweet
+col = db.tweets
+json = import_simplejson()
 
 #Atlanta bounding box including Cobb & Acworth
 loc = [-84.730682,33.502469,-83.94104,34.114079]
@@ -15,13 +23,22 @@ consumer_secret=""
 access_token=""
 access_token_secret=""
 
-class TwitterStreamListener(tweepy.StreamListener):
-    def on_data(self, data):
-        print data
-        return True
+class TwitterStreamListener(StreamListener):
+	#input host = here
+	mongoc = MongoClient(host = 'localhost')
+	db = mongoc.natweet
+	col = db.tweets
+	json = import_simplejson()
 
-    def on_error(self, status):
-        print status
+	def on_data(self, data):
+		if data[0].isdigit():
+			pass
+		else:
+			col.insert(json.loads(data))
+			print(json.loads(data))
+
+	def on_error(self, status):
+		return False
 
 l = TwitterStreamListener()
 auth = OAuthHandler(consumer_key, consumer_secret)
