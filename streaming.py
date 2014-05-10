6e2dd8e3667cc461d7e5253f36147b35c1d9a942
Jonathan Howard <j@hovverd.com>
2014-05-10 14:27:12 -0400

    Removed __main__ check
    Former-commit-id: 3ff19f613aaacd3d051e43e61fcc38e005930d08

--- streaming.py
+++ streaming.py
@@ -27,7 +27,6 @@ class StdOutListener(StreamListener):
     def on_error(self, status):
         print status
 
-# if __name__ == '__main__':
 l = StdOutListener()
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
